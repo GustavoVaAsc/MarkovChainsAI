a8f37c025a3bb713475c831b97c60ea5186aac87
GustavoVaAsc <quickgohan98@gmail.com>
2025-04-22 18:06:09 -0600

    I add the steady state calculation for the problem

--- gambling.rs
+++ gambling.rs
@@ -3,30 +3,31 @@ use std::io;
 static P: f64 = 0.49;
 static INV_P: f64 = 0.51;
 
-fn matrix_vector_multiplication(matrix: &Vec<Vec<f64>>, vector: &Vec<f64>) -> Vec<f64> {
-    let n = matrix.len();
+fn vector_matrix_multiplication(vector: &Vec<f64>, matrix: &Vec<Vec<f64>>) -> Vec<f64> {
+    let n = vector.len();
     let m = matrix[0].len();
-    assert_eq!(m, vector.len(), "Matrix and vector dimensions do not match");
+    assert_eq!(n, matrix.len(), "Vector and matrix dimensions do not match");
 
-    let mut result = vec![0.0; n];
-    for i in 0..n {
-        for j in 0..m {
-            result[i] += matrix[i][j] * vector[j];
+    let mut result = vec![0.0; m];
+    for i in 0..m {
+        for j in 0..n {
+            result[i] += vector[j] * matrix[j][i];
         }
     }
     result
 }
 
-fn generate_markov_chain(n: usize, markov_chain: &mut Vec<Vec<f64>>){
-    for i in 1..n {
-        markov_chain[i][i-1] = P;
-        markov_chain[i-1][i] = INV_P;
+fn generate_markov_chain(n: usize, markov_chain: &mut Vec<Vec<f64>>) {
+    for i in 1..(n - 1) {
+        markov_chain[i][i - 1] = INV_P;
+        markov_chain[i][i + 1] = P;
     }
+
+    // absorbing states
     markov_chain[0][0] = 1.0;
-    markov_chain[1][0] = 0.0;
-    markov_chain[n-1][n-1] = 1.0;
-    markov_chain[n-2][n-1] = 0.0;
+    markov_chain[n - 1][n - 1] = 1.0;
 }
+
 
 fn main(){
     let mut input = String::new();
@@ -39,7 +40,13 @@ fn main(){
     let mut state_vector = vec![0.0; n];
 
     generate_markov_chain(n, &mut markov_chain);
+
+    for row in markov_chain.iter(){
+        println!("{:?}",row);
+    }
+
     input.clear();
+
     println!("Write the initial state");
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
@@ -47,7 +54,21 @@ fn main(){
 
     state_vector[state] = 1.0;
 
-    let result_vector = matrix_vector_multiplication(&markov_chain, &state_vector);
+    let mut next_state = state_vector;
+    for _i in 1..1001 {
+        next_state = vector_matrix_multiplication(&next_state,&markov_chain);
 
-    println!("{:?}", result_vector);
+        let sum: f64 = next_state.iter().sum();
+        if sum != 0.0 {
+            for x in next_state.iter_mut() {
+                *x /= sum;
+            }
+        }
+    }
+    println!("Steady state");
+    print!("[ ");
+    for value in next_state.iter(){
+        print!("{:.2} ",value);
+    }
+    println!("]");
 }
